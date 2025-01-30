ac7443dbd2d5a0554ed50f957a5be0074f75a8a2
Ahmed Gazdiev <gazahmed907@gmail.com>
2025-01-30 18:28:26 +0300

    feat(directive): structural directives *ngIf *ngFor *ngSwitchCase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
+import { NgFor, NgIf, NgSwitchCase } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [NgIf, NgFor, NgSwitchCase],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'angular-tasks';
+  toggler: boolean = true;
+
+  users = [
+    { id: 1, name: 'Иван Петров' },
+    { id: 2, name: 'Светлана Сидорова' },
+  ];
 }
