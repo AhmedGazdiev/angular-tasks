294e221ff121fd54d1564091746cd99e40241a4a
Ahmed Gazdiev <gazahmed907@gmail.com>
2025-01-31 11:47:04 +0300

    feat(decorators): viewChild and viewChildren

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { NgTemplateOutlet } from '@angular/common';
+import {
+  AfterViewInit,
+  Component,
+  ComponentRef,
+  ElementRef,
+  QueryList,
+  TemplateRef,
+  ViewChildren,
+} from '@angular/core';
+import { ChildComponent } from './child/child.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [ChildComponent, NgTemplateOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'angular-tasks';
+  // --- ViewChild ---
+  // @ViewChild('paragraph') paragraph!: ElementRef<HTMLParagraphElement>;
+  // @ViewChild('template') template!: TemplateRef<HTMLTemplateElement>;
+  // @ViewChild(ChildComponent, { static: false })
+  // component!: ComponentRef<ChildComponent>;
+
+  // --- ViewChildren ---
+  @ViewChildren('paragraph') paragraph!: QueryList<
+    ElementRef<HTMLParagraphElement>
+  >;
+  @ViewChildren('template') template!: QueryList<
+    TemplateRef<HTMLTemplateElement>
+  >;
+  @ViewChildren(ChildComponent)
+  component!: QueryList<ComponentRef<ChildComponent>>;
+
+  ngAfterViewInit(): void {
+    console.log('paragraph', this.paragraph);
+    console.log('template', this.template);
+    console.log('component', this.component);
+    // this.paragraph.nativeElement.textContent = 'Hello, Angular!';
+    this.component.forEach((item) => console.log('item', item));
+  }
 }
